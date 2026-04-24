a7ae2e8daecc53c8006089338e9fa75e0d063b79
HoangQuan-K49 <23130258@st.hcmuaf.edu.vn>
2026-04-24 10:22:05 +0700

    #fix: loi dÃatabase khong nhan

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -5,12 +5,12 @@ import model.Product;
 import java.util.List;
 
 public class ProductDao extends BaseDao{
-    public List<Product> getFeaturedProducts() {
-        String sql = "select p.productId, p.productName, p.productPrice, p.imageUrl, p.categoryId, c.name AS categoryName from Product p join Category c on p.categoryId = c.categoryId order by p.sold desc limit 8";
-        return getJdbi().withHandle(
+  public List<Product> getFeaturedProducts() {
+       String sql = "select p.productId, p.productName, p.productPrice, p.imageUrl, p.categoryId, c.name AS categoryName from products p join categories c on p.categoryId = c.categoryId order by p.sold desc limit 8";
+      return getJdbi().withHandle(
                 handle ->
-                        handle.createQuery(sql)
+                       handle.createQuery(sql)
                                 .mapToBean(Product.class)
                                 .list());
-    }
+   }
 }
