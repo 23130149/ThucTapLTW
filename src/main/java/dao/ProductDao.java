b77ba0819591d4f29b1d866f726eb3ffce55efe1
Lê Viết Khanh <233659204+23130149@users.noreply.github.com>
2026-04-24 21:24:16 +0700

    fix: lỗi kết nối ở Dao, biến sai ở productdDao và testrun

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class ProductDao extends BaseDao{
   public List<Product> getFeaturedProducts() {
-       String sql = "select p.productId, p.productName, p.productPrice, p.imageUrl, p.categoryId, c.name AS categoryName from products p join categories c on p.categoryId = c.categoryId order by p.sold desc limit 8";
+       String sql = "select p.Product_Id, p.Product_Name, p.Product_Price,  p.Category_Id, c.Name AS categoryName,(select pi.image_url from product_images pi where pi.product_id = p.product_id order by pi.image_id ASC limit 1) as imageUrl from products p join categories c on p.Category_Id = c.Category_Id order by p.Product_Id desc limit 8";
       return getJdbi().withHandle(
                 handle ->
                        handle.createQuery(sql)
