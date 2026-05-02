3bc8f30191a8b82c04bf113d42752b64333ea108
Lê Viết Khanh <233659204+23130149@users.noreply.github.com>
2026-05-02 09:36:03 +0700

    #feat: Add-Cart 100% view còn lỗi chưa hiện message
    * hoàn thành add-cart chưa view

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -8,5 +8,10 @@ import java.util.List;
 public class ProductService {
     ProductDao pdao =new ProductDao();
     public List<Product> getListProduct() {
-        return pdao.getListProduct();}
+        return pdao.getListProduct();
+    }
+
+    public Product getProductById(int id){
+        return pdao.getProductById(id);
+    }
 }
